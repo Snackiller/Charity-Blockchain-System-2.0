6f56c1d11acbb145f1af96da051ca8d70b400098
Weichen Zhang <eizhang@ucdavis.edu>
2024-01-28 23:54:55 -0800

    configuration and readme
    update readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Charity Blockchain System 2.0
-下载[Node.js](https://nodejs.org/en) (20.11.0 LTS即可)。下载好之后右键此电脑，点击属性，点击高级系统设置，点击高级，点击环境变量，在系统变量那里找到Path，双击Path，新建Node.js的路径 (一般是C:\Program Files\nodejs\)。完成之后打开终端输入node -v以及npm -v，如果出现版本号证明安装成功。用VS Code打开Charity Blockchain System 2.0. 输入npm install，输入npm start，输入Y。打开终端，输入cd Charity-Blockchain-System-2.0，输入cd server，输入node server.js。在打开的 http://localhost:3000 页面当中，在Login Page输入账号HowAreYou以及密码IAmFine进入账号 (注意大小写)。
+
+下载[Node.js](https://nodejs.org/en) (20.11.0 LTS 即可)。下载好之后右键此电脑，点击属性，点击高级系统设置，点击高级，点击环境变量，在系统变量那里找到 Path，双击 Path，新建 Node.js 的路径 (一般是 C:\Program Files\nodejs\)。完成之后打开终端输入 node -v 以及 npm -v，如果出现版本号证明安装成功。用 VS Code 打开 Charity Blockchain System 2.0. 输入 npm install，输入 npm start，输入 Y。打开终端，输入 cd Charity-Blockchain-System-2.0，输入 cd server，输入 node server.js。在打开的 http://localhost:3000 页面当中，在 Login Page 输入账号 HowAreYou 以及密码 IAmFine 进入账号 (注意大小写)。
+
+### How to Run
+
+- go to the directory name: Charity BlockchainSystem
+- npm install
+- npm start
+- cd server
+- node server.js
+- go to the website http://localhost:3000
+- on the login page enter the account: HowAreYou
+- password
+
+### How to Use
+
+- after login, you could create the donation request
+- you need to configure the contractAddress under "Desktop/Charity-Blockchain-System-2.0/src/services/Web3Client.js" to have the correct receiving account
--- src/services/Web3Client.js
+++ src/services/Web3Client.js
@@ -1,267 +1,267 @@
-import Web3 from 'web3';
+import Web3 from "web3";
 
 let web3;
 // let selectedAccount;
 
 const contractABI = [
-    {
-        "inputs": [],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "fundIndex",
+        type: "uint256",
       },
       {
-        "anonymous": false,
-        "inputs": [
-          {
-            "indexed": false,
-            "internalType": "uint256",
-            "name": "fundIndex",
-            "type": "uint256"
-          },
-          {
-            "indexed": false,
-            "internalType": "address",
-            "name": "donor",
-            "type": "address"
-          },
-          {
-            "indexed": false,
-            "internalType": "uint256",
-            "name": "amount",
-            "type": "uint256"
-          }
-        ],
-        "name": "DonationReceived",
-        "type": "event"
+        indexed: false,
+        internalType: "address",
+        name: "donor",
+        type: "address",
       },
       {
-        "anonymous": false,
-        "inputs": [
-          {
-            "indexed": false,
-            "internalType": "uint256",
-            "name": "fundIndex",
-            "type": "uint256"
-          },
-          {
-            "indexed": false,
-            "internalType": "string",
-            "name": "name",
-            "type": "string"
-          },
-          {
-            "indexed": false,
-            "internalType": "string",
-            "name": "description",
-            "type": "string"
-          },
-          {
-            "indexed": false,
-            "internalType": "uint256",
-            "name": "goalAmount",
-            "type": "uint256"
-          }
-        ],
-        "name": "FundCreated",
-        "type": "event"
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "DonationReceived",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "fundIndex",
+        type: "uint256",
       },
       {
-        "anonymous": false,
-        "inputs": [
-          {
-            "indexed": false,
-            "internalType": "uint256",
-            "name": "fundIndex",
-            "type": "uint256"
-          }
-        ],
-        "name": "GoalAchieved",
-        "type": "event"
+        indexed: false,
+        internalType: "string",
+        name: "name",
+        type: "string",
       },
       {
-        "inputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          },
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "name": "fundDonors",
-        "outputs": [
-          {
-            "internalType": "address",
-            "name": "donorAddress",
-            "type": "address"
-          },
-          {
-            "internalType": "uint256",
-            "name": "amount",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function",
-        "constant": true
+        indexed: false,
+        internalType: "string",
+        name: "description",
+        type: "string",
       },
       {
-        "inputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "name": "funds",
-        "outputs": [
-          {
-            "internalType": "string",
-            "name": "name",
-            "type": "string"
-          },
-          {
-            "internalType": "string",
-            "name": "description",
-            "type": "string"
-          },
-          {
-            "internalType": "address payable",
-            "name": "manager",
-            "type": "address"
-          },
-          {
-            "internalType": "uint256",
-            "name": "goalAmount",
-            "type": "uint256"
-          },
-          {
-            "internalType": "uint256",
-            "name": "totalDonated",
-            "type": "uint256"
-          },
-          {
-            "internalType": "uint256",
-            "name": "donorCount",
-            "type": "uint256"
-          },
-          {
-            "internalType": "bool",
-            "name": "isGoalAchieved",
-            "type": "bool"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function",
-        "constant": true
+        indexed: false,
+        internalType: "uint256",
+        name: "goalAmount",
+        type: "uint256",
+      },
+    ],
+    name: "FundCreated",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "fundIndex",
+        type: "uint256",
+      },
+    ],
+    name: "GoalAchieved",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
       },
       {
-        "inputs": [],
-        "name": "owner",
-        "outputs": [
-          {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function",
-        "constant": true
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "fundDonors",
+    outputs: [
+      {
+        internalType: "address",
+        name: "donorAddress",
+        type: "address",
       },
       {
-        "inputs": [
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "funds",
+    outputs: [
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "description",
+        type: "string",
+      },
+      {
+        internalType: "address payable",
+        name: "manager",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "goalAmount",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "totalDonated",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "donorCount",
+        type: "uint256",
+      },
+      {
+        internalType: "bool",
+        name: "isGoalAchieved",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_description",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "_goalAmount",
+        type: "uint256",
+      },
+    ],
+    name: "createFund",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "fundIndex",
+        type: "uint256",
+      },
+    ],
+    name: "donate",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+    payable: true,
+  },
+  {
+    inputs: [],
+    name: "getFunds",
+    outputs: [
+      {
+        components: [
           {
-            "internalType": "string",
-            "name": "_name",
-            "type": "string"
+            internalType: "string",
+            name: "name",
+            type: "string",
           },
           {
-            "internalType": "string",
-            "name": "_description",
-            "type": "string"
+            internalType: "string",
+            name: "description",
+            type: "string",
           },
           {
-            "internalType": "uint256",
-            "name": "_goalAmount",
-            "type": "uint256"
-          }
-        ],
-        "name": "createFund",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "inputs": [
+            internalType: "address payable",
+            name: "manager",
+            type: "address",
+          },
           {
-            "internalType": "uint256",
-            "name": "fundIndex",
-            "type": "uint256"
-          }
-        ],
-        "name": "donate",
-        "outputs": [],
-        "stateMutability": "payable",
-        "type": "function",
-        "payable": true
-      },
-      {
-        "inputs": [],
-        "name": "getFunds",
-        "outputs": [
+            internalType: "uint256",
+            name: "goalAmount",
+            type: "uint256",
+          },
           {
-            "components": [
-              {
-                "internalType": "string",
-                "name": "name",
-                "type": "string"
-              },
-              {
-                "internalType": "string",
-                "name": "description",
-                "type": "string"
-              },
-              {
-                "internalType": "address payable",
-                "name": "manager",
-                "type": "address"
-              },
-              {
-                "internalType": "uint256",
-                "name": "goalAmount",
-                "type": "uint256"
-              },
-              {
-                "internalType": "uint256",
-                "name": "totalDonated",
-                "type": "uint256"
-              },
-              {
-                "internalType": "uint256",
-                "name": "donorCount",
-                "type": "uint256"
-              },
-              {
-                "internalType": "bool",
-                "name": "isGoalAchieved",
-                "type": "bool"
-              }
-            ],
-            "internalType": "struct CharityFund.Fund[]",
-            "name": "",
-            "type": "tuple[]"
-          }
+            internalType: "uint256",
+            name: "totalDonated",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "donorCount",
+            type: "uint256",
+          },
+          {
+            internalType: "bool",
+            name: "isGoalAchieved",
+            type: "bool",
+          },
         ],
-        "stateMutability": "view",
-        "type": "function",
-        "constant": true
-      }
+        internalType: "struct CharityFund.Fund[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
 ]; // abi from CharityFund.json file
-const contractAddress = '0x8C553DC5157D74800985F88541Ff1Da65630e987'; // address from deployed contract in Ganache
+const contractAddress = "0x977F816C9b64A735eFF606Dc4184c9a894A8B474"; // address from deployed contract in Ganache
 
 // Initialization and setup
 const initWeb3 = async () => {
@@ -270,19 +270,23 @@ const initWeb3 = async () => {
     web3 = new Web3(window.ethereum);
     try {
       // Request account access if needed
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      await window.ethereum.request({ method: "eth_requestAccounts" });
     } catch (error) {
       console.error("User denied account access", error);
     }
-  } 
+  }
   // Legacy dapp browsers...
   else if (window.web3) {
     web3 = new Web3(window.web3.currentProvider);
-  } 
+  }
   // If no injected web3 instance is detected, fall back to Infura
   else {
     console.error("No web3 provider detected, falling back to Infura");
-    web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/YOUR_INFURA_API_KEY'));
+    web3 = new Web3(
+      new Web3.providers.HttpProvider(
+        "https://mainnet.infura.io/v3/YOUR_INFURA_API_KEY"
+      )
+    );
   }
 };
 
@@ -299,13 +303,15 @@ const createNewFund = async (charityName, description, fundsRequired) => {
     }
 
     const contract = new web3.eth.Contract(contractABI, contractAddress);
-    await contract.methods.createFund(charityName, description, fundsRequired).send({
-      from: accounts[0],
-    });
+    await contract.methods
+      .createFund(charityName, description, fundsRequired)
+      .send({
+        from: accounts[0],
+      });
 
-    console.log('Fund created successfully');
+    console.log("Fund created successfully");
   } catch (error) {
-    console.error('Error creating fund:', error);
+    console.error("Error creating fund:", error);
   }
 };
 
@@ -319,25 +325,24 @@ const donateToFund = async (fundIndex, donationAmount) => {
     if (accounts.length === 0) {
       throw new Error("No account is available");
     }
-    
-    const amountInWei = web3.utils.toWei(donationAmount, 'ether');
+
+    const amountInWei = web3.utils.toWei(donationAmount, "ether");
     const contract = new web3.eth.Contract(contractABI, contractAddress);
     const transactionReceipt = await contract.methods.donate(fundIndex).send({
       from: accounts[0],
-      value: amountInWei
+      value: amountInWei,
     });
 
     if (transactionReceipt && transactionReceipt.status) {
-      console.log('Donation successful');
+      console.log("Donation successful");
       return { success: true, account: accounts[0], amount: donationAmount };
     } else {
-      console.log('Donation transaction failed');
+      console.log("Donation transaction failed");
       return { success: false };
     }
   } catch (error) {
-    console.error('Error making donation:', error);
+    console.error("Error making donation:", error);
   }
 };
-
 
 export { initWeb3, createNewFund, donateToFund, contractABI, contractAddress };
