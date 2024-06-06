0ace2a3d20acff620dbe1eb3dea16ea5a13f9b18
Shuhao Gao <116941308+Snackiller@users.noreply.github.com>
2024-06-06 15:16:12 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/js/Login';
@@ -6,6 +5,7 @@ import Register from './pages/js/Register';
 import Home from './pages/js/Home';
 import CreatePage from './pages/js/CreatePage';
 import DonationPage from './pages/js/DonationPage';
+import PersonalProfile from './pages/js/PersonalProfile';
 import NavBar from './pages/js/NavBar';
 import Logout from './pages/js/Logout';
 import { ChakraProvider } from "@chakra-ui/react";
@@ -32,6 +32,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/create" element={isLoggedIn ? <CreatePage /> : <Navigate to="/login" />} />
           <Route path="/donation/:charityName" element={isLoggedIn ? <DonationPage /> : <Navigate to="/login" />} />
+          <Route path="/profile" element={isLoggedIn ? <PersonalProfile /> : <Navigate to="/login" />} /> {/* Added this line */}
           <Route path="/logout" element={<Navigate to="/login" replace />} />
         </Routes>
       </Router>
